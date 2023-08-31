79f1444f76604721cc6e3ecdb66247db7aac4b67
Dudeiebot <ojoexcel5@gmail.com>
2023-08-31 22:22:22 +0100

    Shell scripts file

--- config/these.sh
+++ config/these.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#used for splitting my window on startup
+#used for splitting my window, when i open tmux new
+#You can crreate a shell script for calling these
 tmux split-window -v -p 25
+
+
+#I attached these to my terminal config file (mine here is .zshrc) for easier experience
+alias anything-you-like="tmux attach -t"
+alias anything-you-like="tmux ls"
